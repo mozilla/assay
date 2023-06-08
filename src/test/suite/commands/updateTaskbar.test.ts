ce18c799bbe155e61afcda46c243ceec0ec4c3bb
Greg Sherman <gsherman@mozilla.com>
2023-06-08 18:05:44 -0400

    to taskbar tests and update filenames

--- src/test/suite/commands/updateTaskbar.test.ts
+++ src/test/suite/commands/updateTaskbar.test.ts
@@ -1,14 +1,15 @@
+import { expect } from "chai";
+import * as fs from "fs";
+import { afterEach, describe, it } from "mocha";
+import * as fetch from "node-fetch";
+import path = require("path");
+import * as sinon from "sinon";
+import * as vscode from "vscode";
+
 import {
   statusBarItem,
   updateTaskbar,
 } from "../../../amo/commands/updateTaskbar";
-import { expect } from "chai";
-import { afterEach, describe, it } from "mocha";
-import * as sinon from "sinon";
-import * as fetch from "node-fetch";
-import * as fs from "fs";
-import path = require("path");
-import * as vscode from "vscode";
 
 describe("updateTaskbar.ts", async () => {
   afterEach(() => {
@@ -26,6 +27,14 @@ describe("updateTaskbar.ts", async () => {
   const fakeActiveEditor2 = {
     document: {
       uri: {
+        fsPath: "root/guid/version/test.js",
+      },
+    },
+  };
+
+  const fakeActiveEditor3 = {
+    document: {
+      uri: {
         fsPath: "root/12345678-1234-1234-1234-123456789abc/version/test.js",
       },
     },
@@ -39,7 +48,7 @@ describe("updateTaskbar.ts", async () => {
 
   it("should return undefined if there is no activeTextEditor", async () => {
     // by default, vscode.window.activeTextEditor is undefined
-    expect(await updateTaskbar()).to.be.undefined;
+    expect(await updateTaskbar()).to.equal(0);
   });
 
   it("should return undefined if there is no root workspaceFolder", async () => {
@@ -51,7 +60,7 @@ describe("updateTaskbar.ts", async () => {
     stub2.returns(undefined);
     sinon.replaceGetter(vscode.workspace, "workspaceFolders", stub2 as any);
 
-    expect(await updateTaskbar()).to.be.undefined;
+    expect(await updateTaskbar()).to.equal(1);
   });
 
   it("should return undefined if there is no guid or version", async () => {
@@ -63,10 +72,10 @@ describe("updateTaskbar.ts", async () => {
     stub2.returns([fakeWorkspaceFolder]);
     sinon.replaceGetter(vscode.workspace, "workspaceFolders", stub2 as any);
 
-    expect(await updateTaskbar()).to.be.undefined;
+    expect(await updateTaskbar()).to.equal(2);
   });
 
-  it("should return undefined if the response status is 404", async () => {
+  it("should return 3 if the response status is 404", async () => {
     const stub: sinon.SinonStub<any[], any> = sinon.stub();
     stub.returns(fakeActiveEditor2);
     sinon.replaceGetter(vscode.window, "activeTextEditor", stub as any);
@@ -81,10 +90,10 @@ describe("updateTaskbar.ts", async () => {
     });
     sinon.replace(fetch, "default", stub3 as any);
 
-    expect(await updateTaskbar()).to.be.undefined;
+    expect(await updateTaskbar()).to.equal(3);
   });
 
-  it("should update the statusBarItem with default guid structure", async () => {
+  it("should update the statusBarItem with custom guid structure", async () => {
     const stub: sinon.SinonStub<any[], any> = sinon.stub();
     stub.returns(fakeActiveEditor2);
     sinon.replaceGetter(vscode.window, "activeTextEditor", stub as any);
@@ -99,7 +108,29 @@ describe("updateTaskbar.ts", async () => {
     });
     sinon.replace(fetch, "default", stub3 as any);
 
-    expect(await updateTaskbar()).to.be.undefined;
+    expect(await updateTaskbar()).to.equal(undefined);
+    expect(statusBarItem.text).to.equal("guid version");
+    expect(statusBarItem.tooltip).to.equal(
+      "https://reviewers.addons-dev.allizom.org/en-US/reviewers/review/guid"
+    );
+  });
+
+  it("should update the statusBarItem with default guid structure", async () => {
+    const stub: sinon.SinonStub<any[], any> = sinon.stub();
+    stub.returns(fakeActiveEditor3);
+    sinon.replaceGetter(vscode.window, "activeTextEditor", stub as any);
+
+    const stub2: sinon.SinonStub<any[], any> = sinon.stub();
+    stub2.returns([fakeWorkspaceFolder]);
+    sinon.replaceGetter(vscode.workspace, "workspaceFolders", stub2 as any);
+
+    const stub3: sinon.SinonStub<any[], any> = sinon.stub();
+    stub3.returns({
+      status: 200,
+    });
+    sinon.replace(fetch, "default", stub3 as any);
+
+    expect(await updateTaskbar()).to.equal(undefined);
     expect(statusBarItem.text).to.equal(
       "{12345678-1234-1234-1234-123456789abc} version"
     );
