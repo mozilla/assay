f76b45f4bfcbff157ac6542b50a76ad57856018e
Greg Sherman <gsherman@mozilla.com>
2023-06-09 13:23:47 -0400

    follows previous

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, afterEach } from "mocha";
+import { expect } from "chai";
+import * as sinon from "sinon";
+import * as fetch from "node-fetch";
+import * as vscode from "vscode";
+import { activate, deactivate } from "../../amo/extension";
+
+describe("extension.ts", () => {
+  const context = {
+    subscriptions: [] as vscode.Disposable[],
+  } as vscode.ExtensionContext;
+
+  it("should activate and register commands and have 4 subscriptions", async () => {
+    await activate(context);
+    const commands = await vscode.commands.getCommands(true);
+    expect(commands).to.include.members(["assay.get"]);
+    expect(commands).to.include.members(["assay.welcome"]);
+    expect(commands).to.include.members(["assay.review"]);
+    expect(context.subscriptions.length).to.equal(4);
+  });
+});
