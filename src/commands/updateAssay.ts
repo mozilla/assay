dae748015124dc48b71f61a59be0f0a5c213ee33
Greg Sherman <gsherman@mozilla.com>
2023-11-10 00:08:50 -0500

    working version installing

--- src/commands/updateAssay.ts
+++ src/commands/updateAssay.ts
@@ -1,11 +1,12 @@
 import { spawn } from "child_process";
+import * as fs from "fs";
 import fetch from "node-fetch";
+import path = require("path");
 import * as vscode from "vscode";
 
 import { showErrorMessage } from "../utils/processErrors";
 
-export async function newVersion() {
-  // check the github page to see if there is a new release
+export async function checkGetNewVersion() {
   const apiUrl = `https://api.github.com/repos/mozilla/assay/releases/latest`;
   const response = await fetch(apiUrl);
 
@@ -21,28 +22,86 @@ export async function newVersion() {
     vscode.extensions.getExtension("mozilla.assay")?.packageJSON.version;
 
   if (latestVersion !== currentVersion) {
-    return json.assets[0].browser_download_url;
+    return {
+      downloadLink: json.assets[0].browser_download_url,
+      version: latestVersion,
+    };
   }
 
   return false;
 }
 
-export async function installNewVersion(downloadUrl: string) {
-  const downloadProcess = spawn("code", ["--install-extension", downloadUrl]);
-  downloadProcess.on("error", (err) => {
-    vscode.window.showErrorMessage(
-      `Assay failed to install new version: ${err.message}`
+export async function downloadVersion(downloadUrl: string) {
+  return await vscode.window.withProgress(
+    { title: "Assay", location: vscode.ProgressLocation.Notification },
+    async function (progress) {
+      progress.report({
+        message: "Downloading Version",
+      });
+
+      const response = await fetch(downloadUrl);
+      if (!response.ok) {
+        throw new Error(
+          `Could not fetch latest version from GitHub: ${response.statusText}`
+        );
+      }
+
+      const extensionPath =
+        vscode.extensions.getExtension("mozilla.assay")?.extensionPath;
+      if (!extensionPath) {
+        throw new Error("Could not find extension path");
+      }
+      const savePath = path.join(extensionPath, "version.vsix");
+
+      const dest = fs.createWriteStream(savePath, { flags: "w" });
+      dest.write(await response.buffer());
+      dest.close();
+
+      if (!fs.existsSync(savePath)) {
+        const errorMessages = {
+          window: {
+            other: `Could not download addon to ${downloadUrl}`,
+          },
+          thrown: {
+            other: "Download failed",
+          },
+        };
+
+        return await showErrorMessage(errorMessages, "other", downloadVersion, [
+          downloadUrl,
+        ]);
+      }
+
+      return savePath;
+    }
+  );
+}
+
+export async function installNewVersion(downloadUrl: string, version: string) {
+  const savePath = await downloadVersion(downloadUrl);
+  const downloadProcess = spawn("code", ["--install-extension", savePath]);
+
+  downloadProcess.on("exit", (code) => {
+    if (code !== 0) {
+      throw new Error(`Could not install addon`);
+    }
+    fs.unlinkSync(savePath);
+    const currentVersion =
+      vscode.extensions.getExtension("mozilla.assay")?.packageJSON.version;
+    console.log(currentVersion, version);
+
+    vscode.window.showInformationMessage(
+      `Assay updated to version ${version} (from ${currentVersion})`
     );
-    return;
   });
-  return true;
 }
 
 export async function updateAssay() {
-  const newVersionLink = await newVersion();
-  if (newVersionLink && (await installNewVersion(newVersionLink))) {
-    vscode.window.showInformationMessage(
-      `Assay has been updated to version ${newVersionLink}, please restart VSCode to complete the update.`
-    );
+  const downloadInfo = await checkGetNewVersion();
+  if (!downloadInfo) {
+    return;
   }
+  const versionLink = downloadInfo.downloadLink;
+  const version = downloadInfo.version;
+  await installNewVersion(versionLink, version);
 }
