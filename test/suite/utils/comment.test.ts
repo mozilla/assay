ed56998216c0d87015495dcb7f13aaeb711f17fb
Christina Lin <chrlin@mozilla.com>
2024-05-15 16:37:28 -0400

    removed irrelevant tests

---
+++ test/suite/utils/comment.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+
+
+describe("comment.ts", () => {
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    describe("", () => {
+        it("should...", async () => {
+            expect(false).to.equal(true);
+          });
+    })
+
+});
