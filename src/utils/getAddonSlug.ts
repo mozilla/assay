aa4f00a98b65da8dfe1284cdf6a3b312b26f5d41
Christina Lin <44586776+chrstinalin@users.noreply.github.com>
2024-06-17 09:19:18 -0400

    Update src/utils/getAddonSlug.ts
    Co-authored-by: Simeon Vincent <svincent@gmail.com>

--- src/utils/getAddonSlug.ts
+++ src/utils/getAddonSlug.ts
@@ -1,3 +1,16 @@
+/**
+ * Extracts an add-on's slug from a URL or other user input. If a slug cannot be
+ * identified, return the input string.
+ *
+ * The addon's slug is assumed to be the string between a well-known delimiter
+ * and an optional slash after the delimiter. For example, in input string of
+ * "...addon/ublock-origin/?utm_source..." would return "ublock-origin".
+ *
+ * Well-known URL delimiters are:
+ * - "addon/" - A normal AMO link.
+ * - "review/" - A listed add-on in the AMO reviewer tools.
+ * - "review-unlisted/" - A unlisted listed add-on in the AMO reviewer tools.
+ */
 export default function getAddonSlug(input: string) {
   const delimiter = input.includes("review/")
     ? "review/"
