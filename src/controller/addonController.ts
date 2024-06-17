af8f1c729a9ec16360d950a9621e2591022f0597
Christina Lin <chrlin@mozilla.com>
2024-06-17 09:20:23 -0400

    getAddonSlug updates from import-reviewer-url

--- src/controller/addonController.ts
+++ src/controller/addonController.ts
@@ -325,17 +325,27 @@ export class AddonController{
   }
 
   /**
-   * Cleans the input so as to leave only the slug.
+   * Extracts an add-on's slug from a URL or other user input. If a slug cannot be
+   * identified, return the input string.
+   *
+   * The addon's slug is assumed to be the string between a well-known delimiter
+   * and an optional slash after the delimiter. For example, in input string of
+   * "...addon/ublock-origin/?utm_source..." would return "ublock-origin".
+   *
+   * Well-known URL delimiters are:
+   * - "addon/" - A normal AMO link.
+   * - "review/" - A listed add-on in the AMO reviewer tools.
+   * - "review-unlisted/" - A unlisted listed add-on in the AMO reviewer tools.
    * @param input The input to clean.
    * @returns The add-on slug.
    */
   private getAddonSlug(input: string) {
-    const delimiter = input.includes("review/")
-      ? "review/"
-      : input.includes("review-unlisted/")
-      ? "review-unlisted/"
-      : "addon/";
-
+    let delimiter = "addon/";
+    if (input.includes("review/")) {
+      delimiter = "review/";
+    } else if (input.includes("review-unlisted/")) {
+      delimiter = "review-unlisted/";
+    }
     return input.includes("/") ? input.split(delimiter)[1]?.split("/")[0] : input;
   }
 
