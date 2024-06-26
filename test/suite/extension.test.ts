df3a2b540bded79763e4294317558f61e2b1b585
Christina Lin <chrlin@mozilla.com>
2024-06-26 12:06:08 -0400

    extension.test.ts

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -1,67 +1,67 @@
-// import { expect } from "chai";
-// import * as fs from "fs";
-// import { describe, it, afterEach, beforeEach } from "mocha";
-// import * as sinon from "sinon";
-// import * as vscode from "vscode";
+import { expect } from "chai";
+import * as fs from "fs";
+import { describe, it, afterEach, beforeEach } from "mocha";
+import * as sinon from "sinon";
+import * as vscode from "vscode";
 
 
-// import { DirectoryController } from "../../src/controller/directoryController";
-// import { UrlController } from "../../src/controller/urlController";
-// import { activate, deactivate } from "../../src/extension";
-// import { AddonTreeDataProvider } from "../../src/model/sidebarTreeDataProvider";
+import { DirectoryController } from "../../src/controller/directoryController";
+import { UrlController } from "../../src/controller/urlController";
+import { activate, deactivate } from "../../src/extension";
+import { AddonTreeDataProvider } from "../../src/model/sidebarTreeDataProvider";
 
-// function makeContext() {
-//   return {
-//     subscriptions: [] as vscode.Disposable[],
-//     globalStorageUri: vscode.Uri.parse("test"),
-//     globalState: {
-//       get: () => undefined,
-//       update: () => undefined,
-//     },
-//   } as unknown as vscode.ExtensionContext;
-// }
+function makeContext() {
+  return {
+    subscriptions: [] as vscode.Disposable[],
+    globalStorageUri: vscode.Uri.parse("test"),
+    globalState: {
+      get: () => undefined,
+      update: () => undefined,
+    },
+  } as unknown as vscode.ExtensionContext;
+}
 
-// describe("extension.ts", () => {
-//   beforeEach(() => {
-//       const workspaceFoldersStub = sinon.stub(vscode.workspace, "workspaceFolders");
-//       workspaceFoldersStub.value([
-//         {
-//           uri: vscode.Uri.parse("test-root-uri"),
-//         },
-//       ]);
-//   });
+describe("extension.ts", () => {
+  beforeEach(() => {
+      const workspaceFoldersStub = sinon.stub(vscode.workspace, "workspaceFolders");
+      workspaceFoldersStub.value([
+        {
+          uri: vscode.Uri.parse("test-root-uri"),
+        },
+      ]);
+  });
   
-//   afterEach(() => {
-//     sinon.restore();
-//   });
+  afterEach(() => {
+    sinon.restore();
+  });
 
-//   it("should deactivate and return undefined", async () => {
-//     const result = deactivate();
-//     expect(result).to.be.undefined;
-//   });
+  it("should deactivate and return undefined", async () => {
+    const result = deactivate();
+    expect(result).to.be.undefined;
+  });
 
-//   it("should load the manifest if launched with the intention to do so.", async () => {
-//     const directoryControllerStub = sinon.stub(DirectoryController.prototype, 'getRootFolderPath');
-//     directoryControllerStub.resolves('test');
-//     const existsSyncStub = sinon.stub(fs, "existsSync");
-//     existsSyncStub.returns(true);
+  it("should load the manifest if launched with the intention to do so.", async () => {
+    const directoryControllerStub = sinon.stub(DirectoryController.prototype, 'getRootFolderPath');
+    directoryControllerStub.resolves('test');
+    const existsSyncStub = sinon.stub(fs, "existsSync");
+    existsSyncStub.returns(true);
 
-//     sinon.stub(vscode.window, 'createTreeView').returns({dispose: () => undefined} as any);
+    sinon.stub(vscode.window, 'createTreeView').returns({dispose: () => undefined} as any);
 
-//     const context = makeContext();
-//     sinon.stub(context.globalState, 'get').withArgs("filePath").returns("test");
+    const context = makeContext();
+    sinon.stub(context.globalState, 'get').withArgs("filePath").returns("test");
 
-//     context.globalState.update = sinon.stub();
-//     const openCachedFileStub = sinon.stub(UrlController.prototype, "openCachedFile");    
+    context.globalState.update = sinon.stub();
+    const openCachedFileStub = sinon.stub(UrlController.prototype, "openCachedFile");    
 
-//     sinon.stub(vscode.window, "registerUriHandler");
-//     sinon.stub(vscode.commands, "registerCommand");
-//     await activate(context);
-//     expect(openCachedFileStub.calledOnce).to.be.true;
-//     const commands = await vscode.commands.getCommands(true);
-//     expect(commands).to.include.members(["assay.get"]);
-//     expect(commands).to.include.members(["assay.welcome"]);
-//     expect(commands).to.include.members(["assay.review"]);
-//     expect(context.subscriptions.length).to.be.greaterThan(10);    
-//   });
-// });
+    sinon.stub(vscode.window, "registerUriHandler");
+    sinon.stub(vscode.commands, "registerCommand");
+    await activate(context);
+    expect(openCachedFileStub.calledOnce).to.be.true;
+    const commands = await vscode.commands.getCommands(true);
+    expect(commands).to.include.members(["assay.get"]);
+    expect(commands).to.include.members(["assay.welcome"]);
+    expect(commands).to.include.members(["assay.review"]);
+    expect(context.subscriptions.length).to.be.greaterThan(10);    
+  });
+});
